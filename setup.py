aaac086c9688adc17ed84f555107c74e0bf74f8f
bougie <bougie@appartland.eu>
2015-02-15 10:28:19 +0100

    create files for creating python package

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='helpgen',
+      version='0.1',
+      description="""Argparse frontend library to help making software
+usage message""",
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 3',
+      ],
+      keyworkds='argparse help usage',
+      url='https://github.com/bougie/helpgen',
+      author='David Hymonnet',
+      author_email='bougie@appartland.eu',
+      license='BSD',
+      packages=['helpgen'],
+      include_package_data=True,
+      zip_safe=False)
